0e161f230bdbcf3dc1e0c3a69145683e13a2832c
anshukittu <divyanshu03@live.com>
2015-08-18 20:05:13 +0000

    Added form partial and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -16,6 +16,14 @@ class ProfilesController < ApplicationController
         end
     end
     
+    def edit
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+    end
+    
+    def update
+    end
+    
     private
     def profile_params
         flash[:success] = "Profile Updated!"
